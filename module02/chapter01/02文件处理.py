851f0ef16101912ca88031a213e0adce6edf4db4
xuyanlinux <liufengjimo@live.com>
2019-05-21 18:12:17 +0800

    code 更新
    module 2 chapter 1 作业初步完成，未添加详细注释

--- module02/chapter01/02文件处理.py
+++ module02/chapter01/02文件处理.py
@@ -26,20 +26,54 @@
 # for line in f:
 #     print(line.strip())
 # f.close()
+#
+# lis_info_sta = ['id','name','age','phone','dept','enroll_date']
+# str ='1,Alex Li,22,13651054608,IT,2013-04-01'
+#
+# dic_info = dict(zip(lis_info_sta,str.split(',')))
 
-lis_info_sta = ['3','4','age','phone','dept','enroll_date']
-str ='1,Alex Li,22,13651054608,IT,2013-04-01'
+# for i in range(len(lis_info_sta)):
+#     print(lis_info_sta[i],str.split(',')[i],end=' ')
+# for key in lis_info_sta:
+#     if lis_info_sta.index(key) == len(lis_info_sta)-1:
+#         print(dic_info[key])
+#     else:
+#         print(dic_info[key],end=',')
+#
+#
+# st = lis_info_sta[0]+'>'+lis_info_sta[1]
+# print(st)
+# flag = eval(st)
+#
+# print(flag)
+# offset = -50
+# f = open(file='staff_table',mode='rb')
+# while True:
+#     f.seek(offset, 2)  # seek(offset, 2)表示文件指针：从文件末尾(2)开始向前50个字符(-50)
+#     lines = f.readlines()  # 读取文件指针范围内所有行
+#     if len(lines) >= 2:  # 判断是否最后至少有两行，这样保证了最后一行是完整的
+#         last_line = lines[-1]  # 取最后一行
+#         break
+    # 如果off为50时得到的readlines只有一行内容，那么不能保证最后一行是完整的
+    # 所以off翻倍重新运行，直到readlines不止一行
+#     offset *= 2
+# f.close()
+# print('最后一行为：' + last_line.decode())
 
-dic_info = dict(zip(lis_info_sta,str.split(',')))
-for key in lis_info_sta:
-    if lis_info_sta.index(key) == len(lis_info_sta)-1:
-        print(dic_info[key])
-    else:
-        print(dic_info[key],end=',')
+str = 'update staff_table set name="Rain Wang" where name = "Alex Li"'
+if '>=' in str or '<=' in str:
+    str = str.replace('>=',' >= ')
+    str = str.replace('<=',' <= ')
+else:
+    str = str.replace('=',' = ')
+    str = str.replace('>',' > ')
+    str = str.replace('<',' < ')
+str = str.replace('  ',' ')
+list_input = str.split(' ')
+for index,i in enumerate(list_input):
+    if '"' in i :
+        list_input[index] = i+' '+list_input[index+1]
+        del list_input[index+1]
 
-
-st = lis_info_sta[0]+'>'+lis_info_sta[1]
-print(st)
-flag = eval(st)
-
-print(flag)
+print(str)
+print(list_input)
--- module02/chapter01/作业_员工信息怎删改查程序.py
+++ module02/chapter01/作业_员工信息怎删改查程序.py
@@ -4,92 +4,135 @@
 # Date: 2019/5/20
 
 import os
-def add_info(name,age,phone,dept,enroll_date):
-    count = 0
-    pho_flag = True
-    f_r = open(file='staff_table', mode='r', encoding='utf-8')
-    for line in f_r:
-        if phone in line:
-            pho_flag = False
-            f_r.close()
-            print("手机号码已存在！")
-            return
-        else:
-            count += 1
-    f_r.close()
-    id = count + 1
-    f = open(file='staff_table',mode='a',encoding='utf-8')
-    f.write('\n{0},{1},{2},{3},{4},{5}'.format(id,name,age,phone,dept,enroll_date))
+
+
+# def add_info(name, age, phone, dept, enroll_data):
+def add_info(*args):
+    offset = -50
+    f = open(file='staff_table', mode='rb')
+    while True:
+        f.seek(offset, 2)  # seek(offset, 2)表示文件指针：从文件末尾(2)开始向前50个字符(-50)
+        lines = f.readlines()  # 读取文件指针范围内所有行
+        if len(lines) >= 2:  # 判断是否最后至少有两行，这样保证了最后一行是完整的
+            last_line = lines[-1].decode(encoding='UTF-8')  # 取最后一行,并用utf-8编码
+            break
+        # 如果off为50时得到的readlines只有一行内容，那么不能保证最后一行是完整的
+        # 所以off翻倍重新运行，直到readlines不止一行
+        offset *= 2
+    f.close()
+    staff_id_last = int(last_line.strip().split(',')[0])
+    staff_id_new = staff_id_last + 1
+    f = open(file='staff_table', mode='a', encoding='utf-8')
+    f.write('\n{0},{1},{2},{3},{4},{5}'.format(staff_id_new, args[0], args[1], args[2], args[3], args[4]))
     f.close()
 
 
-def del_info(id):
-    f_r = open(file='staff_table',mode='r',encoding='utf-8')
-    lines = f_r.readlines()
-    f_r.close()
+def del_update(del_or_update, *args):
+    lis_info_sta = ['staff_id', 'name', 'age', 'phone', 'dept', 'enroll_data']
+    f_r = open(file='staff_table', mode='r', encoding='utf-8')
+    f_w = open(file='staff_table_del', mode='w', encoding='utf-8')
 
-    f_w = open(file='staff_table',mode='w',encoding='utf-8')
-    for line in lines:
-        if id in line:
-            continue
-        f_w.write(line)
-    f_w.close()
-
-
-def update_info(key_tar,new_value,key_refer,refer_value):
-    lis_info_sta = ['id','name','age','phone','dept','enroll_date']
-    f_r = open(file='staff_table',mode='r',encoding='utf-8')
-    f_w = open(file='staff_table_new',mode='w',encoding='utf-8')
-    for line in f_r:
-        dic_info = dict(zip(lis_info_sta,line.split(',')))
-        if dic_info[key_refer] == refer_value:
-            dic_info[key_tar] = new_value
-            f_w.write(','.join(list(dic_info.values())))
-        else:
-            f_w.write(line)
+    for rline in f_r:
+        if del_or_update == 'del':
+            list_rline = rline.split(',')
+            if list_rline[0] == str(args[0]):
+                continue
+            else:
+                f_w.write(rline)
+        elif del_or_update == 'update':
+            dic_info = dict(zip(lis_info_sta, rline.split(',')))
+            if dic_info[args[2]] == args[3]:
+                dic_info[args[0]] = args[1]
+                f_w.write(','.join(list(dic_info.values())))
+            else:
+                f_w.write(rline)
     f_w.close()
     f_r.close()
     os.remove('staff_table')
-    os.rename('staff_table_new','staff_table')
+    os.rename('staff_table_del', 'staff_table')
 
 
-def find(*args,**kwargs):
+def find(*args, **kwargs):
+    def display():
+        if args[0] == '*':
+            for n in range(len(lis_info_sta)):
+                if n == len(lis_info_sta) - 1:
+                    print(lis_info_sta[n] + ':' + list_line[n])
+                else:
+                    print(lis_info_sta[n] + ':' + list_line[n], end=',')
+        else:
+            for key in args:
+                if args.index(key) == len(args) - 1:
+                    print(key + ':' + dic_info[key])
+                else:
+                    print(key + ':' + dic_info[key], end=',')
+
     f_r = open(file='staff_table', mode='r', encoding='utf-8')
-    lis_info_sta = ['id', 'name', 'age', 'phone', 'dept', 'enroll_date']
+    lis_info_sta = ['staff_id', 'name', 'age', 'phone', 'dept', 'enroll_data']
     for line in f_r:
-        dic_info = dict(zip(lis_info_sta, line.split(',')))
+        line = line.strip()
+        list_line = line.split(',')
+        dic_info = dict(zip(lis_info_sta, list_line))
         if kwargs['compute_sign'] == 'like':
             if kwargs['condition'] in dic_info[kwargs['col']]:
-                if args[0] == '*':
-                    print(line)
-                for key in args:
-                    if lis_info_sta.index(key) == len(lis_info_sta) - 1:
-                        print(dic_info[key])
-                    else:
-                        print(dic_info[key], end=',')
+                display()
+            else:
+                continue
+        elif kwargs['compute_sign'] == '=':
+            flag = dic_info[kwargs['col']] == kwargs['condition']
+            if flag:
+                display()
             else:
                 continue
         else:
             compu_str = dic_info[kwargs['col']]+kwargs['compute_sign']+kwargs['condition']
             flag = eval(compu_str)
             if flag:
-                if args[0] == '*':
-                    print(line.strip())
-                    continue
-                for key in args:
-                    if lis_info_sta.index(key) == len(lis_info_sta) - 1:
-                        print(dic_info[key])
-                    else:
-                        print(dic_info[key], end=',')
+                display()
             else:
                 continue
     f_r.close()
 
 
-
-find('*',col='age',compute_sign='>',condition = '22')
-# find('name',col= 'age',compute_sign='>',condition = '22')
+exit_flag = False
 
 
-# add_info('xuyan','30','1888888','engner','20190202')
-# update_info('dept','IT','dept','Market')
+def analyze_input():
+    statement = input('>>>')
+    if statement.strip() == 'q' or 'Q':
+        print("退出程序")
+        return True
+    if '>=' in statement or '<=' in statement:
+        statement = statement.replace('>=', ' >= ')
+        statement = statement.replace('<=', ' <= ')
+    else:
+        statement = statement.replace('=', ' = ')
+        statement = statement.replace('>', ' > ')
+        statement = statement.replace('<', ' < ')
+    statement = statement.replace('  ', ' ')  # 如果符号前后原本就有空格，现在就有两个空格，把双空格换成单空格
+    list_sta = statement.split(' ')
+    for index, i in enumerate(list_sta):
+        if index >= len(list_sta)-1:
+            break
+        else:
+            if '"' in i and '"' in list_sta[index + 1]:
+                list_sta[index] = i + ' ' + list_sta[index + 1]
+                del list_sta[index + 1]
+    if list_sta[0] == 'find':
+        list_find = list_sta[1].split(',')
+        find(*list_find, col=list_sta[5], compute_sign=list_sta[6], condition=list_sta[7].strip('"'))
+    elif list_sta[0] == 'add':
+        args = list_sta[2].split(',')
+        add_info(*args)
+    elif list_sta[0] == 'del':
+        del_update('del', list_sta[-1].strip())
+    elif list_sta[0] == 'update':
+        del_update('update', list_sta[3], list_sta[5].strip('"'), list_sta[7], list_sta[9].strip('"'))
+        # 如果没有换成单空格，最后一个参数会取空格
+    else:
+        print("语句错误，请重新输入")
+    return False
+
+
+while not exit_flag:
+    exit_flag = analyze_input()
